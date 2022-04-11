52e67e5c777c6e647da1a83dad0e6f6facae38ed
Chobbi <victor.perezml@uanl.edu.mx>
2022-04-11 16:58:48 -0500

    GetById in User Services 2

---
+++ atomek-app/src/services/UserServices.js
@@ -0,0 +1,12 @@
+import { axiosBase as axios } from "./Config";
+
+export const GetById = async (id) => {
+    try {
+        const response = await axios.get(`/users/${id}`);
+        console.log(response);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        console.log (err);
+    }
+}
