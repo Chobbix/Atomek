010aa7a980c25bec2d4c504a5967243d9485b03d
Chobbi <victor.perezml@uanl.edu.mx>
2022-04-18 13:51:00 -0500

    cambio de pantallas con los id en la pagina de muro

--- atomek-app/src/components/Slider.js
+++ atomek-app/src/components/Slider.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 import './Estilos/Slider_style.css'
 import Publicacion from "./Publicacion";
 import Crear_grupo from "./Crear_grupo";
@@ -6,7 +6,80 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAlignJustify } from '@fortawesome/free-solid-svg-icons'
 import GruposBloque_style from "./GruposBloque_style";
 import Publicar from "./Publicar";
+import { Link, useParams } from "react-router-dom";
+import { communityGetComunitiesByUser } from '../services/CommunityServices'
+
 const Slider = () => {
+
+    const [userSesion, setUserSesion] = useState();
+    const [communities, setCommunities] = useState([]);
+    const params = useParams();
+    
+    function switchContentMuro(id) {
+        switch(id) {
+            case 'Mi-Muro':
+                return  <div class="contenedor_Muro bloque_contenedor_cursos">
+                            <Publicar />
+
+                            <Publicacion />
+                            <Publicacion />
+                            <Publicacion />
+                        </div>;
+
+            case 'Descubrir':
+                return  <div class="contenedor_Descubrir  " id="Registro">
+                            <br></br>
+                            <h3>Te puede gustar</h3>
+                            <div class="hileras">
+                                <div className="bloque">
+                                    <GruposBloque_style />
+                                </div>
+                                <div className="bloque">
+                                    <GruposBloque_style />
+                                </div>
+                                <div className="bloque">
+                                    <GruposBloque_style />
+                                </div>
+                            </div>
+                            <div class="hileras">
+                                <div className="bloque">
+                                    <GruposBloque_style />
+                                </div>
+                                <div className="bloque">
+                                    <GruposBloque_style />
+                                </div>
+                                <div className="bloque">
+                                    <GruposBloque_style />
+                                </div>
+                            </div>
+                        </div>;
+
+            case 'Crear-Grupo':
+                return  <div class="contenedor_Crear " id="Registro">
+                            < Crear_grupo />
+                        </div>;
+            default:
+                return 'Juan';
+        }
+    }
+
+    async function getInitialInformation() {
+        try {
+            const userJSON = localStorage.getItem("UserSession");
+            const usuario = (JSON.parse(userJSON));
+            const data = await communityGetComunitiesByUser(usuario);
+            setUserSesion(usuario);
+            setCommunities(data);
+        }
+            catch(err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getInitialInformation();
+    }, []);
+
     return (
         <body id="fonditobonito" className="fonditobonito">
             <div class="Container">
@@ -16,32 +89,33 @@ const Slider = () => {
                     <input type="radio" id="Crear" name="categoria" value="diplomas" />
                     <input type="radio" id="Grupos" name="categoria" value="guardado" />
 
-                    <input type="radio" id="Grupo1" name="categoria" value="guardado" />
-                    <input type="radio" id="Grupo2" name="categoria" value="guardado" />
-                    <input type="radio" id="Grupo3" name="categoria" value="guardado" />
-                    <input type="radio" id="Grupo4" name="categoria" value="guardado" />
-                    <input type="radio" id="Grupo5" name="categoria" value="guardado" />
+
                     <input type="radio" id="GrupoAll" name="categoria" value="guardado" />
 
-                    <input type="radio" id="TuGrupo2" name="categoria" value="guardado" />
-                    <input type="radio" id="TuGrupo3" name="categoria" value="guardado" />
-                    <input type="radio" id="TuGrupo4" name="categoria" value="guardado" />
-                    <input type="radio" id="TuGrupo5" name="categoria" value="guardado" />
                     <input type="radio" id="TuGrupoAll" name="categoria" value="guardado" />
 
                     <div className="Grupo">
                         <div class="canvas2">
-                            <label for="Muro" className="item grupo1">
-                                Tu Muro
-                            </label>
-                            <label for="Descubrir" className="item grupo1">
-                                <h4 >Descubrir</h4>
-                            </label>
-                            <label for="Crear" className="item grupo1">
-                                <h4 >Crear Grupo</h4>
-                            </label>
-                            <h1 className="subtitulo">Grupos recientes</h1>
 
+                            <Link to="/atomek/Muro/Mi-Muro">
+                                <label for="Muro" className="item grupo1">
+                                    <h4 >Tu Muro</h4>
+                                </label>
+                            </Link>
+                            <Link to="/atomek/Muro/Descubrir">
+                                <label for="Descubrir" className="item grupo1">
+                                    <h4 >Descubrir</h4>
+                                </label>
+                            </Link>
+                            <Link to="/atomek/Muro/Crear-Grupo">
+                                <label for="Crear" className="item grupo1">
+                                    <h4 >Crear Grupo</h4>
+                                </label>
+                            </Link>
+
+
+
+                            <h1 className="subtitulo">Grupos recientes</h1>
                             <label for="Grupo1" className="item grupo1">
                                 <h6> Los artistas desnutridos</h6>
                             </label>
@@ -57,133 +131,34 @@ const Slider = () => {
                             <label for="GrupoAll" className="item grupo1">
                                 <h6 className="todos"><FontAwesomeIcon icon={faAlignJustify} /> Ver todos tus grupos</h6>
                             </label>
+
+
+
                             <h1 className="subtitulo"> Tus Grupos </h1>
 
+                            {communities.map((com, index) => (
+                                <Link key={index} to={"/atomek/Muro/" + com._id}>
+                                    <label key={index} for={"TuGrupo" + index} className="item grupo1">
+                                            <h6 key={index}>{com.name}</h6>
+                                    </label>
+                                </Link>
+                            ))}
 
-                            <label for="TuGrupo2" className="item grupo1">
-                                <h6> Los musicos  desafinados</h6>
-                            </label>
-                            <label for="TuGrupo4" className="item grupo1">
-                                <h6> Quemamos la cocina </h6>
-                            </label>
-                            <label for="TuGrupo5" className="item grupo1">
-                                <h6>   Bodypaint </h6>
-                            </label>
                             <label for="TuGrupoAll" className="item grupo1">
                                 <h6 className="todos"><FontAwesomeIcon icon={faAlignJustify} /> Ver todos tus grupos</h6>
                             </label>
-
-
                         </div>
                     </div>
 
 
                     <div class="Muro" id="Registro">
-
                         <div class="bloque-contenido">
-
-                            <div class="contenedor_Muro bloque_contenedor_cursos">
-                                <Publicar />
-
-                                <Publicacion />
-                                <Publicacion />
-                                <Publicacion />
-
-
-                            </div>
-                            <div class="contenedor_Descubrir  " id="Registro">
-                                <br></br>
-                                <h3>Te puede gustar</h3>
-                                <div class="hileras">
-                                    <div className="bloque">
-                                        <GruposBloque_style />
-                                    </div>
-                                    <div className="bloque">
-                                        <GruposBloque_style />
-                                    </div>
-                                    <div className="bloque">
-                                        <GruposBloque_style />
-                                    </div>
-                                </div>
-                                <div class="hileras">
-                                    <div className="bloque">
-                                        <GruposBloque_style />
-                                    </div>
-                                    <div className="bloque">
-                                        <GruposBloque_style />
-                                    </div>
-                                    <div className="bloque">
-                                        <GruposBloque_style />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="contenedor_Crear " id="Registro">
-                                < Crear_grupo />
-                            </div>
-                            <div class="contenedor_Grupos  " id="Registro">
-                                <Publicar />
-                                <Publicacion />
-                                <Publicacion />
-                            </div>
-                            <div class="contenedor_Grupo1  " id="Registro">
-                                <Publicar />
-                                <Publicacion />
-                            </div>
-                            <div class="contenedor_Grupo2  " id="Registro">
-                                <Publicar />
-                                <Publicacion />
-                                <Publicacion />
-                                <Publicacion />
-                                <Publicacion />
-                            </div>
-                            <div class="contenedor_Grupo4  " id="Registro">
-                                <Publicar />
-                                <Publicacion />
-                                <Publicacion />
-                            </div>
-                            <div class="contenedor_Grupo5  " id="Registro">
-                                <Publicar />
-                                <Publicacion />
-                            </div>
-                            <div class="contenedor_GrupoAll  " id="Registro">
-                                <div class="hileras">
-                                    <div className="bloque">
-                                        <GruposBloque_style />
-                                    </div>
-                                    <div className="bloque">
-                                        <GruposBloque_style />
-                                    </div>
-                                    <div className="bloque">
-                                        <GruposBloque_style />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="contenedor_TuGrupo2  " id="Registro">
-                                <Publicar />
-                                <Publicacion />
-                                <Publicacion />
-                                <Publicacion />
-
-
-                            </div>
-                            <div class="contenedor_TuGrupo4  " id="Registro">
-                                <Publicar />
-                                <Publicacion />
-                                <Publicacion />
-                            </div>
-                            <div class="contenedor_TuGrupo5  " id="Registro">
-                                <Publicar />
-                                <Publicacion />
-                            </div>
-                            <div class="contenedor_TuGrupoAll  " id="Registro">
-                                rito
-
-                            </div>
+                        {switchContentMuro(params.id)}
                         </div>
                     </div>
                 </div>
             </div>
-        </body >
+        </body>
     );
 };
 
