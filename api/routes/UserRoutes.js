adbf949f0efdc4f3945315c630237d67dde4ec60
Bryan Duarte <stfrisephone@gmail.com>
2022-04-19 19:25:11 -0500

    Added endpoint to update user's image

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -6,6 +6,7 @@ const userController = require("../controllers/UserController");
 router.get("/users/:id", userController.userGetById);
 router.post("/users", userController.userCreate);
 router.put("/users/:id", userController.userUpdate);
+router.put("/users/:id/image", userController.userUpdateImage);
 router.delete("/users/:id", userController.userDelete);
 
 router.post("/login", userController.userLogin);
