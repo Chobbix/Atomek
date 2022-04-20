894e6bb5ebd9b45cbb1f1801a3629979409eb770
Bryan Duarte <stfrisephone@gmail.com>
2022-04-19 19:41:49 -0500

    Added endpoint to update response's image

--- api/routes/ResponseRoutes.js
+++ api/routes/ResponseRoutes.js
@@ -6,6 +6,7 @@ const responseController = require("../controllers/ResponseController");
 router.get("/response/:id", responseController.responseGetById);
 router.post("/response", responseController.responseCreate);
 router.put("/response/:id", responseController.responseUpdate);
+router.put("/response/:id/image", responseController.responseUpdateImage);
 router.delete("/response/:id", responseController.responseDelete);
 
 router.get("/response/:id/subscription", responseController.responsesGetByIdSubscription);
