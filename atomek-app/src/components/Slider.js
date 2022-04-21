0668f2012db3e47d5f948a3ae211a7d2839f1813
AngelC84 <angelcorona84@gmail.com>
2022-04-21 12:19:48 -0500

    Tabla de rachas para seguir
    21-04-2022

--- atomek-app/src/components/Slider.js
+++ atomek-app/src/components/Slider.js
@@ -3,7 +3,7 @@ import './Estilos/Slider_style.css'
 import Publicacion from "./Publicacion";
 import Crear_grupo from "./Crear_grupo";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAlignJustify } from '@fortawesome/free-solid-svg-icons'
+import { faAlignJustify, faPlus } from '@fortawesome/free-solid-svg-icons'
 import GruposBloque_style from "./GruposBloque_style";
 import Publicar from "./Publicar";
 const Slider = () => {
@@ -85,11 +85,47 @@ const Slider = () => {
                             <div class="contenedor_Muro bloque_contenedor_cursos">
                                 <Publicar />
 
-                                <Publicacion />
-                                <Publicacion />
-                                <Publicacion />
+                                <div className="my-3 p-3 rounded border" id="RecuadroRachas">
+                                <h6 className="text-light border-bottom pb-2 mb-0 ">Rachas sugeridas de Usuarios</h6>
+                                <div className="d-flex text-muted pt-3">
+                                <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6998AB"/><text x="50%" y="50%" fill="#6998AB" dy=".3em">32x32</text></svg>
 
+                                <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
+                                <div className="d-flex justify-content-between">
+                                <strong className="text-light">Racha del mejor Abogado</strong>
+                                <a className="text-light"><FontAwesomeIcon icon={faPlus}/></a>
+                                </div>
+                                <span className="d-block">@Lic PugBerto</span>
+                                </div>
+                                </div>
 
+                                <div className="d-flex text-muted pt-3">
+                                <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6998AB"/><text x="50%" y="50%" fill="#6998AB" dy=".3em">32x32</text></svg>
+
+                                <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
+                                <div className="d-flex justify-content-between">
+                                <strong className="text-light">Racha del mejor crochet cada día</strong>
+                                <a className="text-light"><FontAwesomeIcon icon={faPlus}/></a>
+                                </div>
+                                <span className="d-block">Mi abuelita no me enseño a coser</span>
+                                </div>
+                                </div>
+
+                                <div className="d-flex text-muted pt-3">
+                                <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6998AB"/><text x="50%" y="50%" fill="#6998AB" dy=".3em">32x32</text></svg>
+
+                                <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
+                                <div className="d-flex justify-content-between">
+                                <strong className="text-light">Racha de programar como un iiiinche toro sin usar pastillas</strong>
+                                <a className="text-light"><FontAwesomeIcon icon={faPlus}/></a>
+                                </div>
+                                <span className="d-block">@Chobbi</span>
+                                </div>
+                                </div>
+                                </div>
+                                <Publicacion />
+                                <Publicacion />
+                                <Publicacion />
                             </div>
                             <div class="contenedor_Descubrir  " id="Registro">
                                 <br></br>
