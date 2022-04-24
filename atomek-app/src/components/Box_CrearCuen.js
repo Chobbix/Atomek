eb7685a45eeb24d52edeafc80586f15183cc906e
Bryan Duarte <stfrisephone@gmail.com>
2022-04-23 22:17:57 -0500

    Removed Login and Register using Google and Facebook

--- atomek-app/src/components/Box_CrearCuen.js
+++ atomek-app/src/components/Box_CrearCuen.js
@@ -89,38 +89,6 @@ const Box_login = () => {
         <div class="my-3">
             <span>Ya tienes cuenta? <Link to="/atomek/login">Ingresa aquí</Link></span><br></br>
         </div>
-  
-          <div className="container w-100 my-4">
-              <div className="row text-center">
-                  <div className="col-12">Crear cuenta con::</div>
-              </div>
-              <div className="row">
-                  <div className="col">
-                      <button className="btn btn-outline-primary w-100 my-1">
-                          <div className="row align-items-center">
-                              <div className="col-2 d-none d-md-block">
-                                  <img src={fb} width="32" alt=""></img>
-                              </div>
-                              <div className="col-12 col-md-10 text-center">
-                                  Facebook
-                              </div>
-                          </div>
-                      </button>
-                  </div>
-                  <div className="col">
-                      <button className="btn btn-outline-danger w-100 my-1">
-                          <div className="row align-items-center">
-                              <div className="col-2 d-none d-md-block">
-                                  <img src={google} width="32" alt=""></img>
-                              </div>
-                              <div className="col-12 col-md-10 text-center">
-                                  Google
-                              </div>
-                          </div>
-                      </button>
-                  </div>
-              </div>
-          </div>
       </div>
       </div>
       </div>
