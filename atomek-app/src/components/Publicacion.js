845c77ec1a1ae35df8393fd6c2701910f87b8981
Bryan Duarte <stfrisephone@gmail.com>
2022-04-23 22:33:31 -0500

    Changed date format on posts

--- atomek-app/src/components/Publicacion.js
+++ atomek-app/src/components/Publicacion.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import './Estilos/Publicacion_style.css'
-import pp from '../Imagenes/picture_perfil.jpg'
 import Carrousel_publicacion from './carrousel_publicacion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPeopleCarry, faStar } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom";
+import Moment from 'moment';
 
 const Publicacion = (props) => {
+    Moment.locale('es');
+
     return (
         <div class="tarjetamuro ">
             <div class="cardheader">
                 <div>
                     <Link to={"/atomek/Perfil/" + props.propPost._user?._id}>
-                    <img src={pp} alt="img-avatar" className='perfil'></img>
+                    <img src={props.propPost._user?.image  ?? `https://avatars.dicebear.com/api/bottts/${props.propPost._user?._id}.svg`} alt="img-avatar" className='perfil'></img>
                     </Link>
                 </div>
                 <div>
                     <h4 className='grupo'>{props.propPost._community?.name}</h4>
                     <div className='informacion'>
                         <h5 className='usuario' >{props.propPost._user?.username} </h5>
-                        <h5 className='Fecha' > {props.propPost?.date_create}</h5>
+                        <h5 className='Fecha' > {Moment(props.propPost?.date_create).format('DD/MM/yyyy')}</h5>
                     </div>
                 </div>
 
