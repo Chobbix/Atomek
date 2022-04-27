8c1df236b74dc676519b07bd50fdcdd4afa76fee
Chobbi <victor.perezml@uanl.edu.mx>
2022-04-27 07:41:04 -0500

    creacion de funcion validateUsername y aplicada en el muro

---
+++ atomek-app/src/functions/validateUserSession.js
@@ -0,0 +1,6 @@
+async function validateUserSession() {
+    console.log("Soy una funcion");
+    return true;
+}
+
+export default validateUserSession
--- atomek-app/src/pages/Muro.js
+++ atomek-app/src/pages/Muro.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Box_muro from '../components/Box_muro'
+import { useNavigate } from 'react-router-dom';
+import validateUserSession from '../functions/validateUserSession';
 
 const Muro = () => {
+  const Navigate = useNavigate();
+
+  useEffect(() => {
+    if (validateUserSession()) { Navigate('/atomek/login') }
+  }, []);
+
   return (
     <Box_muro/>
   )
